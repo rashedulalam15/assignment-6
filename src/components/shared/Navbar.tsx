e9a2b993af4ba678551dcc8f2acdf85ff687379e
Rashedulman <rashedulalam2k02@gmail.com>
2026-09-24 18:39:17 +0600

    added navbar component

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import logo from '@/assets/logo.png'
+
+const Navbar = () => {
+    return (
+        <div className='bg-base-100'>
+            <div className="navbar  shadow-sm container mx-auto">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg aria-label="Menu" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul
+        tabIndex={-1}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li ><Link href="/">Workouts</Link></li>
+      <li><Link href="/myPlan">My Plan</Link></li>
+      </ul>
+    </div>
+     <div className='flex gap-2 items-center'>
+        <Image src={logo} alt='navbar logo'></Image>
+        <h2 className='font-semibold text-2xl'>FITLOG</h2>
+     </div>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><Link href="/">Workouts</Link></li>
+      <li><Link href="/myPlan">My Plan</Link></li>
+    </ul>
+  </div>
+  <div className="navbar-end ">
+    <a className="btn rounded-2xl">Plan<span className='h-5 w-5 flex items-center justify-center rounded-full text-black bg-[#C2F800]'>0</span></a>
+    <a className="btn rounded-2xl">Saved<span className='h-5 w-5 border border-gray-300 flex items-center justify-center rounded-full'>0</span></a>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Navbar;
